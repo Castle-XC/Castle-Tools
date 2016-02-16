0f794436a39451c333647c87b64b7690024becba
Chris <Castle>
2016-02-16 09:42:13 -0800

    Removing some old example files

--- FE-Tools/scripts/example2.js
+++
@@ -1,7 +0,0 @@
-
-// Example JavaScript File #2
-(function(window, document, $, undefined) {
-    "use strict";
-
-    console.log("Example JavaScript File #2 has been successfully loaded.");
-})(window, document, jQuery);
